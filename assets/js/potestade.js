d4c73e1555ba832cfbaa3c144935aa74234371ab
TuSoporteApp <84728004+tusoporteapp@users.noreply.github.com>
2024-02-09 12:47:42 -0500

    Update potestade.js

--- assets/js/potestade.js
+++ assets/js/potestade.js
@@ -3,7 +3,7 @@
    const readyToast = new bootstrap.Toast(toastEl)
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("/assets/js/service-worker.min.js", { scope: "/assets/js/index" })
+      .register("/assets/js/service-worker.min.js", { scope: "/assets/js/index.html" })
       .then(() => readyToast.show());
   } else {
     console.log("CLIENT: service worker is not supported.");
